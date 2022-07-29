ec0821485802e11c6dbe7b0e761f4a73b7ce82a7
PlaYCoNecT <andin.cell.1987@gmail.com>
2022-07-29 09:12:54 -0300

    Add files via upload

---
+++ install/ubuinst1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9wbGF5Y29uZWN0L3B3ZWIvcGFpbmVsL3VidWluc3Quc2ggJiYgY2htb2QgK3ggdWJ1aW5zdC5zaCAmJiBkb3MydW5peCB1YnVpbnN0LnNoID4gL2Rldi9udWxsIDI+JjEKY2QgfHwgZXhpdA==' | base64 -d | bash
+/bin/ubuinst.sh
